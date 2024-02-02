595088d24d944259231b03f75193cfa51260eb1a
GNLaForgia-Bruner <93950533+GNLaForgia-Bruner@users.noreply.github.com>
2024-02-02 06:59:19 -0600

    v1.0
    Initial release; v1.0

---
+++ BG3LST.py
@@ -0,0 +1,32 @@
+def ask_completion():
+    print("Baulder's Gate 3 - Post-Sleep Tracker")
+    print("Open Source & Free - Please support Larian Studios")
+    print("Created by: Apollo - Discord: Gruffalo#0000\n")
+
+    options = ['A. Longstrider', 'B. Warding Bond', 'C. Bardic Inspiration', 'D. Potions/Elixirs']
+    responses = {}
+
+    for option in options:
+        response = input(f"Have you performed {option}? (yes/no): ").lower()
+        while response != 'yes':
+            if response == 'no':
+                print("You must perform this task before proceeding.")
+            else:
+                print("Invalid input. Please enter 'yes' or 'no'.")
+            response = input(f"Have you performed {option}? (yes/no): ").lower()
+        responses[option] = response
+
+    reset = input("Would you like to reset for the next long rest? (yes/no): ").lower()
+    while reset != 'yes':
+        if reset == 'no':
+            print("You must reset for the next long rest.")
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+        reset = input("Would you like to reset for the next long rest? (yes/no): ").lower()
+
+    print("\nResetting responses...")
+    ask_completion()
+
+if __name__ == "__main__":
+    ask_completion()
+
